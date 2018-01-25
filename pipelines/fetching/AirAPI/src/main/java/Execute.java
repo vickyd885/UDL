4189f3a73532b1bb574def0fa6251cc787cee21a
vickyd885 <vickyd885@gmail.com>
2018-01-25 14:03:23 +0000

    add some mock http requests code

--- pipelines/fetching/AirAPI/src/main/java/Execute.java
+++ pipelines/fetching/AirAPI/src/main/java/Execute.java
@@ -22,5 +22,33 @@ public class Execute{
     HttpClient client = new HttpClient();
     GetMethod method = new GetMethod(API_URL_ENTRY);
 
+    method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+           new DefaultHttpMethodRetryHandler(3, false));
+
+    try {
+     // Execute the method.
+     int statusCode = client.executeMethod(method);
+
+     if (statusCode != HttpStatus.SC_OK) {
+       System.err.println("Method failed: " + method.getStatusLine());
+     }
+
+     // Read the response body.
+     byte[] responseBody = method.getResponseBody();
+
+     // Deal with the response.
+     // Use caution: ensure correct character encoding and is not binary data
+     System.out.println(new String(responseBody));
+
+   } catch (HttpException e) {
+     System.err.println("Fatal protocol violation: " + e.getMessage());
+     e.printStackTrace();
+   } catch (IOException e) {
+     System.err.println("Fatal transport error: " + e.getMessage());
+     e.printStackTrace();
+   } finally {
+     // Release the connection.
+     method.releaseConnection();
+   }
   }
 }
